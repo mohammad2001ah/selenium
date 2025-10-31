bbec73e26fcfcae3694659b8973fe9a53e948888
mohammad2001ah <mah.2001.ah@gmail.com>
2025-10-31 18:21:43 +0300

    todo

--- src/test/java/findElemnentBy/FindElementByID.java
+++ src/test/java/findElemnentBy/FindElementByID.java
@@ -11,18 +11,18 @@ public class FindElementByID {
     WebDriver driver =new ChromeDriver();
 
     driver.get("file:D:\\Selenium\\firstClass\\src\\test\\resources\\index.html");
-    String s = driver.findElement(By.id("app-header")).getText();
-    String  s2 = driver.findElement(By.name("app-description")).getText();
-    String s3 =driver.findElement(By.className("list1")).getText();
+//    String s = driver.findElement(By.id("app-header")).getText();
+//    String  s2 = driver.findElement(By.name("app-description")).getText();
+//    String s3 =driver.findElement(By.className("list1")).getText();
 
     //to find multiple classes we use cssSelector(element.name1.name2....)
-    String s33 =driver.findElement(By.cssSelector("li.list4.web.mobile")).getText();
-    //fid element by tag name
-    String s4 =driver.findElement(By.tagName("li")).getText();
-    //find element by LinkText
-    String s5 =driver.findElement(By.linkText("We will use this website to learn selenium commands")).getText();
-    //find element by LinkText
-    String s6 =driver.findElement(By.partialLinkText("We will use this website to")).getText();
+//    String s33 =driver.findElement(By.cssSelector("li.list4.web.mobile")).getText();
+//    //fid element by tag name
+//    String s4 =driver.findElement(By.tagName("li")).getText();
+//    //find element by LinkText
+//    String s5 =driver.findElement(By.linkText("We will use this website to learn selenium commands")).getText();
+//    //find element by LinkText
+//    String s6 =driver.findElement(By.partialLinkText("We will use this website to")).getText();
     //find element by xpath
 
     /**
@@ -30,12 +30,14 @@ public class FindElementByID {
      * //tagName[@attribute='value']
      * //*[@attribute='value']
      * */
-    String s7 =driver.findElement(By.xpath("/html/body/ul[1]/li[3]")).getText();
-    String s8 =driver.findElement(By.xpath("//ul[@class='course-list']//li[3]")).getText();
-    String s9 =driver.findElement(By.xpath("//*[@class='country-list']")).getText();
+//    String s7 =driver.findElement(By.xpath("/html/body/ul[1]/li[3]")).getText();
+//    String s8 =driver.findElement(By.xpath("//ul[@class='course-list']//li[3]")).getText();
+//    String s9 =driver.findElement(By.xpath("//*[@class='country-list']")).getText();
 
+    //findElements
+    String s10=driver.findElements(By.cssSelector(".course-list li")).get(7).getText();
 
-    System.out.println(s9);
+    System.out.println(s10);
 
 
 
